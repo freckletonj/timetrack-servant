0952f0bbad37b73fc015412d2e3c4add479ca845
Josh <freckletonj@gmail.com>
2017-02-18 16:33:55 -0700

    use bad ass Kleisli Composition to solve it!
    I feel great, having just rediscovered Kleisli Comp.
    
    That is all.

--- src/Api/User.hs
+++ src/Api/User.hs
@@ -21,6 +21,8 @@ import Data.Time              (UTCTime, getCurrentTime, addUTCTime)
 import Data.Time.Clock.POSIX  (posixSecondsToUTCTime)
 import Data.Typeable          (Typeable)
 
+import Control.Arrow
+import Control.Monad
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger   (runStdoutLoggingT, logError)
 import Control.Monad.Reader
@@ -50,6 +52,7 @@ import Database.Persist.Postgresql (runSqlPool
                                    , withPostgresqlPool
                                    , liftSqlPersistMPool
                                    , PersistValue (PersistText)
+                                   , PersistEntity
                                    , Entity
                                    , SqlBackend
                                    , selectList)
@@ -57,11 +60,11 @@ import Database.Persist.Postgresql (runSqlPool
 import Database.Persist.Sql (rawSql, SqlPersistT, unSingle, Single)
 import Database.Persist.TH
 
+import Crypto.JOSE.Error (Error)
 import Crypto.KDF.BCrypt (hashPassword, validatePassword)
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as BL
 
--- import Control.Arrow (left)
 import Data.Bifunctor
 import Control.Monad.Trans.Either
 
@@ -69,47 +72,63 @@ import Models
 import Config
 import Lib
 
+-- TODO: belongs in config
+hashIterations = 12         -- 15 =~ 6 sec
+tokenDuration = 60*60*24*30 -- one month, TODO: make revokable, expiration works, but can't be revoked that way
+
+--------------------------------------------------
+-- Login API
+
 data UnsafeLogin = UnsafeLogin { email :: String
                                , clearPass :: String} deriving (Generic)
-instance FromJSON UnsafeLogin
-
-
+instance FromJSON UnsafeLogin 
 
 type LoginAPI = "login" :> ReqBody '[JSON] UnsafeLogin
                         :> Post '[JSON] (String)
               :<|> "new" :> ReqBody '[JSON] UnsafeLogin
                          :> Post '[JSON] (Key Login)
 
--- TODO: belongs in config
-hashIterations = 12 -- 15 =~ 6 sec
-tokenDuration = 60*60*24*30 -- one month, TODO: make revokable, expiration works, but can't be revoked that way
-
--- returnJwt :: JWTSettings -> String -> IO (Either Error BL.ByteString)
+returnJwt :: (ToJWT a) =>  JWTSettings -> a -> IO (Either Error BL.ByteString)
 returnJwt jwts obj = do
-  -- generate JWT
+
   now <- getCurrentTime
   let expiry = addUTCTime tokenDuration now
   etoken <- makeJWT obj
             jwts
             (Just expiry)
   return $ etoken
+
+createKey :: (PersistEntity record) => String -> App (Key record)
+createKey email = do
+  k  <- return (keyFromValues [PersistText $ pack email])
+  k' <- either (const $ throwError err401) return k
+  return k'
+
+fetchPass :: (Key Login) -> App String
+fetchPass k = do
+  mu           <- (runDb (get k))
+  (Login _ hp) <- maybe (throwError err401) return mu
+  return hp
+
+validateHash :: String -> String -> App ()
+validateHash p hp = if validatePassword (B.pack p) (B.pack hp) then return () else throwError err401
+
+getToken :: (ToJWT a) => JWTSettings -> a -> App String
+getToken jwts obj = do
+  etoken <- liftIO (returnJwt jwts obj)
+  either (\_ -> throwError err401) (return . B.unpack . BL.toStrict) etoken
+
   
 loginServerT :: JWTSettings -> ServerT LoginAPI App
 loginServerT jwts = login :<|> new
   where
     login :: UnsafeLogin -> App String
-    login (UnsafeLogin email p) = do
-      k <- return (keyFromValues [PersistText $ pack email])
-      k' <- either (const $ throwError err401) return k
-      
-      mu <- (runDb (get k'))
-      (Login _ hp) <- maybe (throwError err401) return mu
-
-      if validatePassword (B.pack p) (B.pack hp) then return () else throwError err401
-      etoken <- liftIO (returnJwt jwts (User email Nothing Nothing))
-      either (\_ -> throwError err401) (return . B.unpack . BL.toStrict) etoken
-
-
+    login (UnsafeLogin email p) = createKey
+                                  >=> fetchPass
+                                  >=> (validateHash p)
+                                  >=> const (getToken jwts (User email Nothing Nothing)) -- TODO: return a better object
+                                  $ email -- this ends up as `createKey`'s param
+     
     new :: UnsafeLogin -> App (Key Login)
     new (UnsafeLogin e p) = do
       k <- runDb . insert $ (User e Nothing Nothing)
@@ -120,12 +139,6 @@ loginServerT jwts = login :<|> new
 maybeToEither :: err -> Maybe a -> Either err a
 maybeToEither = flip maybe Right . Left
 
-
-           
-  
-              
-
-
 loginServerToHandler :: Config -> App :~> ExceptT ServantErr IO
 loginServerToHandler cfg = Nat (flip runReaderT cfg . runApp)
 
@@ -133,8 +146,8 @@ loginServer :: JWTSettings -> Config -> Server LoginAPI
 loginServer jwts cfg = enter (loginServerToHandler cfg) (loginServerT jwts)
 
 
-
-----------
+--------------------------------------------------
+-- User API
 
 type UserAPI = Get '[JSON] [Entity User]                 -- list all - todo: auth+role
                 :<|> ReqBody '[JSON] User
@@ -171,31 +184,3 @@ userServerToHandler cfg = Nat (flip runReaderT cfg . runApp)
 
 userServer :: Config -> Server UserAPI
 userServer cfg = enter (userServerToHandler cfg) userServerT
-
-
--- Reduction of the Login Problem
--- :: Int -> Either String Int
-f = (\x -> if x>0          then Right x else Left "too small")
-g = (\x -> if x<100        then Right x else Left "too big")
-h = (\x -> if rem x 2 == 0 then Right x else Left "no odds")
-
-thing :: (MonadIO m) => m String
-thing = do
-  a <- fmap read $ liftIO getLine
-  case f a of
-    Left x -> error x
-    Right b ->
-      case g b of
-        Left x -> error x
-        Right c ->
-          case h c of
-            Left x -> error x
-            Right d ->
-              return $ show . (+1) $ d
-              
-thing' :: (MonadIO m) => m String
-thing' = do
-  a <- fmap read $ liftIO getLine
-  case (f a) >>= g >>= h of
-    Left e -> error e
-    Right x -> return $ show x
